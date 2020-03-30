7d27dadb18ee8559baba527c4b0d8fb2ede847c3
jantapabh <s6035512034@phuket.psu.ac.th>
2020-03-30 12:41:20 +0700

    add commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useState, useEffect } from 'react'
+import axios from 'axios'
+import StudentCard from './components/StudentCard'
+import StudentList from './components/StudentList'
+import InputForm from './components/InputForm'
+import { useSelector, useDispatch, Provider } from 'react-redux'
 
-function App() {
+import LoginForm from './components/LoginForm'
+import { store } from './redux/store'
+import { bindActionCreators } from 'redux'
+import { AuthActions, StudentActions } from '../src/redux/store';
+
+
+
+axios.defaults.withCredentials = true
+
+
+
+// App เป็นส่วนหลักในการไปยังหน้าต่าง ๆ 
+
+const App = () => {
+
+  const [loading, setLoading] = useState(true)
+  const auth = useSelector(state => state.Auth);
+  const actions = bindActionCreators({ ...StudentActions, ...AuthActions }, useDispatch());
+
+  useEffect(() => {
+
+    actions.getLoginStatus().then(res => setLoading(false));
+
+    // เมือ่โหลดหน้าเสร็จให้ไปที่ url ที่เก็บ session อยู่
+
+  }, []);
+
+  if (loading)
+    return "Loading ..."
+
+  if (!auth.accessToken && !auth.psuInfo)
+
+    return (
+      <Provider>
+      <div>
+        <LoginForm />
+      </div>
+      </Provider>
+    )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
+      <Provider>
+           <div>
+      <StudentList />
+      <InputForm />
+      <button onClick={() => actions.logout()}>Log Out!!</button>
     </div>
-  );
+    </Provider>
+
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
+import {BrowserRouter , Router, Switch, Link} from 'react-router-dom';
+import StudentList from './components/StudentList'
+import InputForm from './components/InputForm';
+import LoginForm from './components/LoginForm'
+
+
+const routing = (
+  
+        <div>
+            <App />
+        </div>
+ 
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
