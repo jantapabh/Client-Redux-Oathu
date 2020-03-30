076f41378a975734be922eeee191c6ca007fcd49
jantapabh <s6035512034@phuket.psu.ac.th>
2020-03-30 12:44:47 +0700

    add commit

--- src/App.js
+++ src/App.js
@@ -1,61 +1,17 @@
-import React, { Component, useState, useEffect } from 'react'
-import axios from 'axios'
-import StudentCard from './components/StudentCard'
-import StudentList from './components/StudentList'
-import InputForm from './components/InputForm'
-import { useSelector, useDispatch, Provider } from 'react-redux'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
 
-import LoginForm from './components/LoginForm'
-import { store } from './redux/store'
-import { bindActionCreators } from 'redux'
-import { AuthActions, StudentActions } from '../src/redux/store';
-
-
-
-axios.defaults.withCredentials = true
-
-
-
-// App เป็นส่วนหลักในการไปยังหน้าต่าง ๆ 
-
-const App = () => {
-
-  const [loading, setLoading] = useState(true)
-  const auth = useSelector(state => state.Auth);
-  const actions = bindActionCreators({ ...StudentActions, ...AuthActions }, useDispatch());
-
-  useEffect(() => {
-
-    actions.getLoginStatus().then(res => setLoading(false));
-
-    // เมือ่โหลดหน้าเสร็จให้ไปที่ url ที่เก็บ session อยู่
-
-  }, []);
-
-  if (loading)
-    return "Loading ..."
-
-  if (!auth.accessToken && !auth.psuInfo)
-
-    return (
-      <Provider>
-      <div>
-        <LoginForm />
-      </div>
-      </Provider>
-    )
-
-  return (
- 
-      <Provider>
-           <div>
-      <StudentList />
-      <InputForm />
-      <button onClick={() => actions.logout()}>Log Out!!</button>
-    </div>
-    </Provider>
-
-  )
-}
-
-export default App;
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
