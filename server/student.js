c5926c0c0d9776a02561f481049a4a32b89ff50f
jantapabh <s6035512034@phuket.psu.ac.th>
2020-03-30 12:13:35 +0700

    add commit

--- server/student.js
+++ server/student.js
@@ -169,7 +169,6 @@ router.route('/auth/psu')
 .get(psuRoute.login);
 
 
-
 app.use("*", (req, res) => res.status(404).send('404 Not found'));
 
 app.listen(8000, () => { console.log('server is running') })
