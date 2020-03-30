8e62bb9f859f1e22f77db094633ffd8136e981e9
jantapabh <s6035512034@phuket.psu.ac.th>
2020-03-30 12:58:27 +0700

    add loginForm

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -12,11 +12,12 @@ import { bindActionCreators } from 'redux'
 import config from '../config'
 
 
-const LoginForm = () => {
+const LoginForm = (props) => {
 
     const actions = bindActionCreators(AuthActions, useDispatch());
     const [facebookLink, setFacebookLink] = useState('');
-
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
     const getFacebookLink = async () => {
 
         const res = await axios.get(`${config.apiUrl}/auth/facebook`);
@@ -34,21 +35,61 @@ const LoginForm = () => {
         actions.loginPSU(username.value, password.value)
     }
     return (
-
         <div>
-            <div>
-                Facebook Login Click <a href={facebookLink}>Here</a>
-            </div>
-            <h1>PSU PASSPORT</h1>
-            <div>
-                <form onSubmit={loginPSU}>
-                    Username: <input type="text" name="username" placeholder="Put Usename" /> <br />
-                    PassWord: <input type="password" name="password" placeholder="Put Password" /> <br />
-                    <input type="submit" value="Submit" />
-                </form>
+            <div className="d-flex justify-content-center">
+                <div className="d-flex flex-column bd-highlight mb-2">
+                    <h1>HELLO LOGIN HW8</h1>
+                    <div>
+                        <Card style={{ width: '20rem' }}>
+                            <Card.Img variant="top" src="https://www.beartai.com/wp-content/uploads/2015/07/facebook_2015_logo_detail.png" />
+                            <Card.Body>
+                                <Card.Title>Facebook LOGIN</Card.Title>
+                                <Card.Text>
+                                    Arim Cheberahim
+                                    6035512059
+                            </Card.Text>
+                                <Button variant="outline-primary" href={facebookLink}>LOGIN</Button>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                    {' '}
+                    <div>
+                        <div className="">
+                            <form>
+                                <div>
+                                    <div>
+                                        {/* USERNAME : <input type="text" name="username" />
+                            PASSWORD : <input type="password" name="password" /> */}
+                                        <div>
+                                            <Card style={{ width: '20rem' }}>
+                                                <Card.Img variant="top" src="https://www.phuket.psu.ac.th/wp-content/uploads/2019/03/cropped-PSU_PHUKET-EN.png" />
+                                                <Card.Body>
+                                                    <Card.Title>PSU LOGIN</Card.Title>
+                                                    <Card.Text>
+                                                        <Form style={{ width: '18rem' }}>
+                                                            <Form.Group controlId="formBasicEmail">
+                                                                <Form.Label>Username</Form.Label>
+                                                                <Form.Control type="text" placeholder="Enter Username" name="username" onChange={(e)=>setUsername(e.target.value)}/>
+                                                            </Form.Group>
+                                                            <Form.Group controlId="formBasicPassword">
+                                                                <Form.Label>Password</Form.Label>
+                                                                <Form.Control type="password" placeholder="Password" name="password" onChange={(e)=>setPassword(e.target.value)}/>
+                                                            </Form.Group>
+                                                        </Form>
+                                                    </Card.Text>
+                                                    <Button variant="outline-primary" onClick={LoginPSU}>LOGIN</Button>
+                                                </Card.Body>
+                                            </Card>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
-}
 
+}
 export default LoginForm;
